c0fc71ac7d08025b2d1553b8909e27594c534dd9
adttilove <1963096826@qq.com>
2025-11-01 22:14:09 +0800

    Support play task

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # YiCaoPu
+[![Marketplace Version](https://img.shields.io/visual-studio-marketplace/v/adttil.yicaopu.svg)](https://marketplace.visualstudio.com/items/adttil.yicaopu)
+[![installs](https://img.shields.io/visual-studio-marketplace/i/adttil.yicaopu.svg)](https://marketplace.visualstudio.com/items/adttil.yicaopu)
+[![license](https://img.shields.io/github/license/Adttil/yicaopu-vscode.svg)](https://github.com/Adttil/yicaopu-vscode/blob/main/LICENSE.txt)
+[![issues](https://img.shields.io/github/issues/Adttil/yicaopu-vscode.svg)](https://github.com/Adttil/yicaopu-vscode/issues)
 
-Support for `YiCaoPu`, a simple plain-text music score format.
+Support for `YiCaoPu`, a simple text-based music score format.
 
 ## Features
 
-1. Syntax highlighting for `YiCaoPu`
-2. Convert `YiCaoPu` files to `midi` files.
+### Syntax highlighting for `YiCaoPu`.
+![syntax highlight](doc/highlight-example.png)
+
+
+### Play selection in `YiCaoPu` file.
+![play selection](doc/play-selection-example.png)
+    In the context menu or press `F5` to play the selection.
+
+
+### Convert `YiCaoPu` files to `midi` files.
+![convert to midi](doc/convert-to-midi-example.png)
+    This will generate a `MIDI` file with the same name.
--- src/extension.ts
+++ src/extension.ts
@@ -1,34 +1,30 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import { buffer } from 'stream/consumers';
 
-let converter_path: string;
+let yicaopu_path: string;
 
 export function activate(context: vscode.ExtensionContext) {
-    converter_path = path.join(context.extensionPath, 'tool', "yicaopu2midi.exe");
+    yicaopu_path = path.join(context.extensionPath, 'tool', "yicaopu.exe");
 
-	const disposable = vscode.commands.registerCommand('yicaopu.toMidi', 
-		async (uri?: vscode.Uri) => {
-		try{ 
-			await processFile(uri);
-		}
-		catch(error){}
-	});
+	const disposables = [
+        vscode.commands.registerCommand('yicaopu.toMidi', convertToMidi),
+        vscode.commands.registerCommand('yicaopu.play', play),
+        vscode.commands.registerCommand('yicaopu.playSelection', playSelection)
+    ];
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(...disposables);
 }
 
 export function deactivate() {}
 
-async function processFile(uri?: vscode.Uri): Promise<void>
+async function convertToMidi(uri?: vscode.Uri): Promise<void>
 {
 	const editor = vscode.window.activeTextEditor;
 	const filePath = editor?.document.fileName as string;
-	await executeExternalProcessor([filePath], `process: ${filePath}`);
-}
 
-async function executeExternalProcessor(filePaths: string[], taskName: string): Promise<void> {
-
-    const task = createTask(filePaths, taskName);
+    const args = [filePath, filePath.slice(0, filePath.length - 4) + '.mid'];
+	const task = createTask(args, `to midi: ${filePath}`);
     
     const execution = await vscode.tasks.executeTask(task);
     
@@ -44,11 +40,62 @@ async function executeExternalProcessor(filePaths: string[], taskName: string): 
     await taskEndPromise;
 }
 
-function createTask(filePaths: string[], taskName: string): vscode.Task {
-    const args = [...filePaths];
+async function play(uri?: vscode.Uri): Promise<void>
+{
+	const editor = vscode.window.activeTextEditor;
+	const filePath = editor?.document.fileName as string;
+
+    const temp_path = filePath.slice(0, filePath.length - 4) + '.temp.mid';
+    const args = [filePath, temp_path, 'path'];
+	const task = createTask(args, `play: ${filePath}`);
     
+    const execution = await vscode.tasks.executeTask(task);
+    
+    const taskEndPromise = new Promise<void>((resolve) => {
+        const disposable = vscode.tasks.onDidEndTask((e) => {
+            if (e.execution === execution) {
+                disposable.dispose();
+                resolve();
+            }
+        });
+    });
+
+    await taskEndPromise;
+}
+
+async function playSelection(uri?: vscode.Uri): Promise<void>
+{
+	const editor = vscode.window.activeTextEditor as vscode.TextEditor;
+    
+    const doc = editor.document;
+    const start = doc.offsetAt(editor.selection.start);
+    
+    const select = doc.getText(editor.selection);
+    const before = doc.getText().substring(0, start);
+
+	const filePath = editor?.document.fileName as string;
+
+    const temp_path = filePath.slice(0, filePath.length - 4) + '.temp.mid';
+    const args = [filePath,`${Buffer.byteLength(before, 'utf8')}`, `${Buffer.byteLength(select, 'utf8')}`, temp_path];
+	const task = createTask(args, `play selection`);
+    
+    const execution = await vscode.tasks.executeTask(task);
+    
+    const taskEndPromise = new Promise<void>((resolve) => {
+        const disposable = vscode.tasks.onDidEndTask((e) => {
+            if (e.execution === execution) {
+                disposable.dispose();
+                resolve();
+            }
+        });
+    });
+
+    await taskEndPromise;
+}
+
+function createTask(args: string[], taskName: string): vscode.Task {    
     const taskExecution = new vscode.ProcessExecution(
-        converter_path,
+        yicaopu_path,
         args
     );
 
@@ -56,7 +103,7 @@ function createTask(filePaths: string[], taskName: string): vscode.Task {
     	{ type: 'process' },
         vscode.TaskScope.Workspace,
         taskName,
-        'Yicaopu to Midi',
+        'YiCaoPu',
         taskExecution
     );
 
